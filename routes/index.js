edd257f1c76161aedf964256c479ff17bb9fa6b7
hamzaboutaleb <hamza-boutaleb@live.fr>
2024-02-14 17:20:01 +0100

    fix issues

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthControllers.getConnect);
 router.get('/disconnect', AuthControllers.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.get('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
